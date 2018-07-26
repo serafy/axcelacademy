7e46904d475751c71ad227e114b1d6a30a92172f
Amr <amrspy2023@gmail.com>
2018-07-26 11:15:06 +0300

    this is my first try

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String msg = "this is my first try";
         System.out.println("this my first try") ;
+
     }
 }
