36f570f462458367fbc51b4dca2b763ac5687f44
ahmedel-serafy <ahmed@serafy.co.uk>
2018-07-30 00:15:03 +0300

    Adding the input from keyboard

--- src/com/axcel/Main.java
+++ src/com/axcel/Main.java
@@ -1,5 +1,7 @@
 package com.axcel;
 
+import java.util.Scanner;
+
 /**
  * This is a runner class
  */
@@ -11,8 +13,41 @@ public class Main {
      */
     public static void main(String[] args) {
 	// write your code here
-        String msg = "this is my 1st test";
+        String msg = "This is a Test program";
+        System.out.println(msg);
+        System.out.println();
+
+
+
+        int[] array = readArray();
+        printArray(array);
+
+
+
+    }
+
+    private static int[] readArray(){
+        String msg = "Enter the number of elements to read:";
         System.out.println(msg);
 
+        Scanner inputScanner = new Scanner(System.in);
+
+        int length = inputScanner.nextInt();
+
+        msg = "Enter "+ length +" elements to read:";
+        System.out.println(msg);
+        int[] array = new int[length];
+        for (int i=0;i<length;i++){
+            array[i] = inputScanner.nextInt();
+        }
+        return array;
+
+    }
+
+    private static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
     }
 }
